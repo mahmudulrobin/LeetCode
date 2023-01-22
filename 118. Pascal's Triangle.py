e2976fbc41ce15fbfb08741a109613f9e8e2d1f9
Mahmudul Hasan Robin <mahmudulrobin17@gmail.com>
2023-01-22 01:21:25 -0600

    Solved: https://leetcode.com/problems/pascals-triangle/solutions/3084893/dynamic-programming-python-solution/

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        output=[[1]]
+        for i in range(1,numRows):
+            temp=[0]+output[-1]+[0]
+            row=[]
+            for j in range(len(output[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            output.append(row)
+        return output
+
+
+
