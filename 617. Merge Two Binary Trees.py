f01cbb995a7872340ca43aa02404255d6c950ca7
Mahmudul Hasan Robin <mahmudulrobin17@gmail.com>
2023-02-26 23:59:11 -0600

    Solved: https://leetcode.com/problems/merge-two-binary-trees/solutions/3235207/simple-python-solution/

---
+++ 617. Merge Two Binary Trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1 and not root2:
+            return None
+        elif not root1:
+            root = root2
+        elif not root2:
+            root = root1
+        else:
+            root = TreeNode(root1.val + root2.val)
+            root.left = self.mergeTrees(root1.left, root2.left)
+            root.right = self.mergeTrees(root1.right, root2.right)
+        return root
